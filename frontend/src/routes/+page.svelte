1f0a840d9d7dc7fdda62e060a3f8fe29fdfa69aa
Mukund <saivishnumukund@outlook.com>
2024-07-01 17:44:24 -0500

    random trials

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,23 @@
 <script>
-  import { Alert } from 'flowbite-svelte';
+  import { onMount } from 'svelte';
+  import { Alert, Badge, Indicator } from 'flowbite-svelte';
+  import { invoke } from '@tauri-apps/api/tauri';
+import { P, GradientButton } from 'flowbite-svelte';
+  let endpoints = false;
+  let available='Unavailable';
+  let color = 'red';
+  async function test_endpoints(){
+    endpoints = invoke('test_endpoints', { });
+    if(endpoints){
+      color = 'green';
+      available = 'Available';
+
+    }
+    else{
+      color = 'red';
+      available = 'Unavailable';
+    }
+  }
 </script>
 <html class="dark" lang="en">
 <body width="100vw" height="100vh">
@@ -9,5 +27,13 @@
     Change a few things up and try submitting again. Hi my name is muund
   </Alert>
 </div>
+    <div on:load="{test_endpoints}" style="padding-left:5%;">
+      <P size="base">IPFS facilities are: </P> <Badge color="{color}" rounded class="px-2.5 py-0.5">
+        <Indicator color="{color}" size="xs" class="me-1" />{available}
+      </Badge>
+    </div>
+    <div style="padding-left: 5%;">
+      <GradientButton on:click="{test_endpoints}">Re-Test</GradientButton>
+    </div>
     </body>
   </html>
