4335bc73d14842982efdcb80512b3dd881f98d8c
Mukund <saivishnumukund@outlook.com>
2024-07-01 16:18:18 -0500

    in between

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <div class="p-8">
   <Alert>
     <span class="font-medium">Info alert!</span>
-    Change a few things up and try submitting again.
+    Change a few things up and try submitting again. Hi my name is muund
   </Alert>
 </div>
     </body>
