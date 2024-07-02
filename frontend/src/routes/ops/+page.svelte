7b4cfc123d69b9076693573cdbcd44c47115833c
Mukund <saivishnumukund@outlook.com>
2024-07-02 11:52:51 -0500

    doin package tab

--- frontend/src/routes/ops/+page.svelte
+++ frontend/src/routes/ops/+page.svelte
@@ -1,20 +1,46 @@
-<script>
-  import { Tabs, TabItem } from 'flowbite-svelte';
-  import { ArchiveSolid, ArrowDownToBracketOutline } from 'flowbite-svelte-icons';
+<script lang="ts">
+  import { Tabs, TabItem, Button, Toast } from 'flowbite-svelte';
+  import { ArchiveSolid, ArrowDownToBracketOutline, CloseCircleSolid } from 'flowbite-svelte-icons';
+  import { invoke } from '@tauri-apps/api/tauri';
+  import { fly } from 'svelte/transition';
+  let result = null;
+  let counter = 5;
+  let toastStatus = false;
+  let copyStatus = false;
+  async function _package(){
+    invoke('log_message', {message: "package is running"});
+    result = await invoke('package_existing_file_mfs', { } );
+    if(result == null){
+      toastStatus = true;
+      timeout();
+    }
+    else{
+      
+    }
+  }
+  function timeout(){
+    if (--counter > 0) return setTimeout(timeout, 1000);
+    toastStatus = false;
+  }
 </script><html class="dark" lang="en">
   <body width="100vw" height="100vh">
-    <Tabs>
+<Tabs>
   <TabItem open>
     <div slot="title" class="flex items-center gap-2">
       <ArchiveSolid size="md" />
       Package
     </div>
-
-
-
     
 
 
+    <Button on:click="{_package}" pill>Get Started!</Button>
+    <Toast color="red" dismissable={true} transition={fly} params={{x:200}} bind:toastStatus>
+      <svelte:fragment slot="icon">
+        <CloseCircleSolid class="w-5 h-5" />
+        <span class="sr-only">Error icon</span>
+      </svelte:fragment>
+      Not valid
+    </Toast>
   </TabItem>
 
 
@@ -39,6 +65,6 @@
 
 
 
-    </Tabs>
+</Tabs>
   </body>
 </html>
